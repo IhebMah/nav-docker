da01d524d83adb41d157a89776db34ee0acb4fc0
Iheb Mahmoudi <47473024+IhebMah@users.noreply.github.com>
2024-05-02 14:51:49 +0400

    Revert "New CDN (#550)"

--- generic/Run/pscoreoverrides.ps1
+++
@@ -1,3 +0,0 @@
-﻿function Invoke-SqlCmd { SqlServer\Invoke-Sqlcmd @args -Encrypt Optional }
-function Backup-SqlDatabase { SqlServer\Backup-SqlDatabase @args -Encrypt Optional }
-function Restore-SqlDatabase { SqlServer\Restore-SqlDatabase @args -Encrypt Optional }
